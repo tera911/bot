132d22fae1341c810dee244c4b0fd1b9c4435cf9
tera911.mainPC <t_hanyu_kudo@yahoo.co.jp>
2013-07-31 08:57:59 +0900

    add jquery-1.10.2

--- tweet.php
+++ tweet.php
@@ -6,7 +6,7 @@
 <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
 <link href="./bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no,initial-scale=1,maximum-scale=1">
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="./js/jquery-1.10.2.min.js"></script>
 
 <style type="text/css">
 <!--
